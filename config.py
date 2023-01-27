c2a5aeffd6c1caaf10b149969e13886940a0c4b8
Claire <garot.claire@live.fr>
2023-01-27 09:45:14 +0100

    ajout cdn css

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ class Config:
     DEBUG = True,  # some Flask specific configs
     CACHE_TYPE = "SimpleCache"  # Flask-Caching related configs
     CACHE_DEFAULT_TIMEOUT = 86400  # 24 hours cache timeout
-    SERVER_NAME= 'geek-compagnon.io:5000'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://claire:XR9V9vAwXX@192.168.1.9:5432/gk'
