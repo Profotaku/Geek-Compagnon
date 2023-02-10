c6ee2ec914e22b79ff2e67a7263ae9860b1d4a4d
Claire <garot.claire@live.fr>
2023-02-10 14:27:29 +0100

    Ajout input tag, input tag selected element debut ajour ean13 formulaire test

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://claire:XR9V9vAwXX@192.168.1.9:5432/gk'
     REMEMBER_COOKIE_DURATION = timedelta(days=30)
     SECRET_KEY = 'ZSgmA*4FS*%e$Y69tgb!^mb7q'
+    JWT_SECRET = 'ZSgmA*4FS*%e$Y69tgb!^mb7q'
     JWT_AUTHMAXAGE = 3600
     JWT_REFRESHMAXAGE = 604800
     JWT_ISSUER = "Geek-Compagnon"
