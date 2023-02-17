28f67ded49df49ba11590dba085aa57cbfafd9d9
Claire <garot.claire@live.fr>
2023-02-17 10:59:15 +0100

    début navbar.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'chartmagenta': '#7e3288',
+      'chartblue' : '#5395e4',
+      'chartpink' : '#c78bce',
+      'chartpurple' : '#7a87d2',
+    },
+    mode: 'jit',
+    purge: [
+     './template/public/**/*.html',
+     './template/public/*.html',
+     './static/css/*.css',
+     './static/js/*.{js,jsx,ts,tsx,vue}',
+   ],
   },
   plugins: [
     require('flowbite/plugin')
